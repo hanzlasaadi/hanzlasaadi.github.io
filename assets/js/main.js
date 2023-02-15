3b6db7ed18277ca1f4b171413c74a24dd9d81766
hanzlasaadi <hanzalasaadi@gmail.com>
2023-02-16 01:01:59 +0500

    form updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,7 +136,7 @@ function scrollActive() {
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
+    const sectionId = current.getAttribute("id");
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document
