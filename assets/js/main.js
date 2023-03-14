931aef2c9245bcc514c852b826cbea3031cd3513
hanzlasaadi <hanzalasaadi@gmail.com>
2023-03-14 18:03:52 +0500

    lazy loading portfolio gifs

--- assets/js/main.js
+++ assets/js/main.js
@@ -204,3 +204,29 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("blur");
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "1000px",
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
